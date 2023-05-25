84da59b2a2a5c0eb59143b132ca2bcbc0661f666
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2023-05-25 00:58:21 -0700

    Add functionality for calculating a letter grade based on the percentage, and calculating the total count of students for each letter grade

--- src/main/resources/static/js/calculate.js
+++ src/main/resources/static/js/calculate.js
@@ -1,24 +1,69 @@
 const boundsContainer = document.querySelector(".bounds");
 
-const defaultGrades =  [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03,
-    49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
+const grades = [
+  65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.0, 81.43, 86.22, 88.33,
+  9.03, 49.93, 52.34, 53.11, 50.1, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54,
+  90.0, 71.11, 80.01,
+];
 
 const gradeToPercentage = new Map([
-    ["Max", 100.00],
-    ["A+", 95.00 ],
-    ["A", 90.00],
-    ["A-", 85.00 ],
-    ["B+", 80.00],
-    ["B", 75.00 ],
-    ["B-", 70.00],
-    ["C+", 65.00 ],
-    ["C", 60.00],
-    ["C-", 55.00 ],
-    ["D", 50.00],
-    ["F+", 0.00 ]
-])
+  ["Max", 100.0],
+  ["A+", 95.0],
+  ["A", 90.0],
+  ["A-", 85.0],
+  ["B+", 80.0],
+  ["B", 75.0],
+  ["B-", 70.0],
+  ["C+", 65.0],
+  ["C", 60.0],
+  ["C-", 55.0],
+  ["D", 50.0],
+  ["F+", 0.0],
+]);
 
-gradeToPercentage.forEach((val, key) => {
+const gradeCount = new Map([
+  ["A+", 0],
+  ["A", 0],
+  ["A-", 0],
+  ["B+", 0],
+  ["B", 0],
+  ["B-", 0],
+  ["C+", 0],
+  ["C", 0],
+  ["C-", 0],
+  ["D", 0],
+  ["F+", 0],
+]);
+
+const calculateCountForEachGrade = () => {
+  resetGradeCount();
+  grades.forEach((grade) => {
+    const letterGrade = getLetterGrade(grade);
+    const previousCount = gradeCount.get(letterGrade);
+    gradeCount.set(letterGrade, previousCount + 1);
+  });
+};
+const resetGradeCount = () => {
+  [...gradeCount.keys].forEach((key) => gradeCount.set(key, 0));
+};
+const getLetterGrade = (grade) => {
+  const ranges = [...gradeToPercentage.values];
+  for (let i = 0; i < ranges.length - 1; i++) {
+    const percentage = ranges[i];
+    const nextLetterGradePercentage = ranges[i + 1];
+    if (grade >= percentage && grade < nextLetterGradePercentage) {
+      gradeToPercentage.forEach((val, key) => {
+        if (val == percentage) {
+          return key;
+        }
+      });
+    }
+  }
+  return "Max";
+};
+
+const renderLowerBounds = () => {
+  gradeToPercentage.forEach((val, key) => {
     const boundContainer = document.createElement("div");
     boundContainer.className = "bound";
     const boundLabel = document.createElement("label");
@@ -27,16 +72,21 @@ gradeToPercentage.forEach((val, key) => {
     const boundInput = document.createElement("input");
     boundInput.setAttribute("type", "number");
     boundInput.setAttribute("id", "adjustBound");
-    boundInput.className = "adjustBound"
+    boundInput.className = "adjustBound";
     boundInput.setAttribute("step", "any");
     boundInput.value = val;
+    boundInput.setAttribute("min", "0");
+    boundInput.setAttribute("max", "100");
     boundContainer.appendChild(boundLabel);
     boundContainer.appendChild(boundInput);
     boundsContainer.append(boundContainer);
-})
+  });
+};
 
-{/* <div class = "bound">
+{
+  /* <div class = "bound">
                     <label for = "adjustBound">
                         Max
                     </label>
-                    <input type="number" id = "adjustBound"></input> */}
+                    <input type="number" id = "adjustBound"></input> */
+}
